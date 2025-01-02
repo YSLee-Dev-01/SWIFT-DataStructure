d61a8c3efde5d7f192a8243a3ff0209dfc940ca2
YSLee-Dev <ysleedev01@gmail.com>
2025-01-02 22:24:48 +0900

    Study: Queue의 정의 추가 및 복습

--- 1. D-Queue.playground/Contents.swift
+++ 1. D-Queue.playground/Contents.swift
@@ -1,5 +1,20 @@
 import UIKit
 
+/// 1. Queue
+/// 한쪽 끝에서만 삽입이 이루어지고, 다른 한쪽 끝에서는 삭제만 이루어지는 자료구조
+/// 삭제 연산이 되는 곳을 Front, 산입 연산이 이루어지는 곳을 Rear라고 함
+/// Queue는 FIFO 구조를 가지고 있음
+/// - 물건을 사기 위해 기다리고 있는 사람들
+///
+/// Queue는 DeQueue를 통해 데이터를 get (제거) 할 수 있고,
+/// enQueue를 통해 데이터를 삽입할 수 있음
+///
+/// deQueue를 통해 데이터를 제거할 시 앞에 데이터가 빠지기 때문에 Front가 뒤로 밀려나게 됨
+/// -> Queue의 가용범위가 줄어듬 (Rear는 밀리지 않기 때문에)
+/// -> Front와 Rear가 동일한 주소를 바라보게 될 경우 Queue는 사용이 불가능하게 됨
+/// 해당 문제는 원형Queue를 사용하여 해결할 수 있음
+
+
 // Queue 자료구조
 struct Queue<T> {
     private var queue: [T] = []
