8caded6d5828802ed64cf2cf6b3a4d67889a5c18
YSLee-Dev <ysleedev01@gmail.com>
2025-01-06 21:57:28 +0900

    Study: LinkedList 구현 복습

--- 3. D-LinkedList.playground/Contents.swift
+++ 3. D-LinkedList.playground/Contents.swift
@@ -38,117 +38,242 @@ import UIKit
 // 단방향 연결 리스트(Linked List)
 
 class Node<T: Equatable> {
-    var data: T?
-    var next: Node?
+    var data: T? // 현재 데이터를 나타내는 프로퍼티
+    var next: Node? // 다음 순서 데이터의 메모리를 가르키는 프로퍼티
     
     init(data: T, next: Node? = nil) {
         self.data = data
         self.next = next
     }
-    
-    deinit {
-        print("DEINIT--\(data)")
-    }
 }
 
 class LinkedList<T: Equatable> {
-    private var head: Node<T>?
+    /// 연결리스트는 각각 떨어진 공간에 데이터를 연결하여 사용하기 떄문에 첫 Node를 가르키는 Head가 필수적
+    private var head: Node<T>? // 첫 Node를 가르키는 프로퍼티
     
     func append(data: T) {
-        // head가 없을 때
-        guard let _ = self.head else {
+        if self.head == nil { // Head가 없을 경우 Head를 지정
             self.head = Node(data: data)
             return
         }
         
-        // head를 기본으로 두고 연결 list의 끝 == node.next가 nil일 때까지 순회
-        var node = self.head
-        while node?.next != nil {
-            node = node?.next
+        var nowNode = self.head
+        while nowNode?.next != nil { // next 값이 없으면 가장 마지막 Node로 간주
+            nowNode = nowNode?.next
         }
-        let newNode = Node(data: data)
-        node?.next = newNode
+        
+        nowNode?.next = Node(data: data)
     }
     
-    func insert(data: T, index: Int) {
-        guard let _ = self.head else {
+    func insert(data: T, at index: Int) {
+        if self.head == nil { // Head가 없을 경우 Head를 지정
             self.head = Node(data: data)
             return
         }
         
-        var node = self.head
-        // index에서 1을 뺀 만큼 순회
-        for _ in 0 ..< (index - 1) {
-            if node?.next == nil {break}
-            node = node?.next
+        var nowNode = self.head
+        for _ in 0 ..< index - 1 { // 해당 index의 전 Node까지만 이동하여 순서를 변경함
+            nowNode = nowNode?.next
         }
-        let nextNode = node?.next
-        node?.next = Node(data: data)
-        node?.next?.next = nextNode
+        var newNode = Node(data: data, next: nowNode?.next)
+        nowNode?.next = newNode
+    }
+    
+    func searchNode(data: T) -> Node<T>? {
+        var nowNode = self.head
+        while nowNode != nil {
+            if nowNode?.data == data {
+                return nowNode
+            }
+            nowNode = nowNode?.next
+        }
+        return nil // 만약 전체를 돌았는데도 값이 나오지 않는 경우 nil이 반환됨
     }
     
     func removeLast() {
-        guard self.head != nil else {return}
-        guard self.head?.next != nil else { self.head = nil; return}
+        if self.head == nil {return}
+        if self.head?.next == nil {self.head = nil; return}
         
-        var node = self.head
-        // node의 마지막 값까지는 들어가지 않도록 > 자기 자신을 지울 수 없음
-        while node?.next?.next != nil {
-            node = node?.next
+        var nowHead = self.head
+        while nowHead?.next?.next != nil { // 현재 Head의 next의 next가 없다는 건 Head의 바로 이후 next가 마지막이라는 뜻
+            nowHead = nowHead?.next
         }
-        node?.next = nil
+        nowHead?.next = nil
     }
     
-    func remove(index: Int) {
-        guard self.head != nil else {return}
-        guard index != 0 || self.head?.next == nil else {
+    func remove(at index: Int) {
+        if self.head == nil {return}
+        if index == 0 {
             self.head = self.head?.next
             return
         }
         
-        var node = self.head
-        for _ in 0 ..< (index - 1) {
-            if node?.next?.next == nil {break}
-            node = node?.next
+        var nowHead = self.head
+        for _ in 0 ..< index - 1 {
+            nowHead = nowHead?.next
+            if nowHead == nil {return} // 삭제를 원했던 index가 잘못되어 nil 값인 경우 아무런 작업을 하지 않음
         }
-        
-        let nextValue = node?.next?.next
-        node?.next = nextValue
-    }
-    
-    func searchNode(data: T) -> Node<T>? {
-        guard self.head != nil else {return nil}
-        guard self.head?.data != data else {return self.head}
-        
-        var node = self.head
-        while node?.next != nil {
-            if node?.data == data {
-                break
-            }
-            node = node?.next
-        }
-        
-        return node
+        nowHead?.next = nowHead?.next?.next
     }
 }
+
 
 var intList = LinkedList<Int>()
+intList.append(data: 0) // 0
+let zero = intList.searchNode(data: 0)
+print(zero?.data, zero?.next?.data)
 
-for item in 1 ... 10 {
-    intList.append(data: item)
-}
+intList.append(data: 1) // 0, 1
 
-print(intList.searchNode(data: 5)?.data)
+let trashValue = intList.searchNode(data: 99)
+print(trashValue?.data, trashValue?.next)
+
+intList.insert(data: 2, at: 2) // 0, 1, 2
+
+let one = intList.searchNode(data: 1)
+print(one?.data, one?.next?.data)
+let two = intList.searchNode(data: 2)
+print(two?.data, two?.next)
+
+
+intList.insert(data: 9, at: 1)
+let nine = intList.searchNode(data: 9) // 0, 9, 1, 2
+print(nine?.data, nine?.next?.data)
 
 intList.removeLast()
+let removeLastData = intList.searchNode(data: 1)
+print(removeLastData?.data, removeLastData?.next?.data) // 0, 9, 1
 
-print(intList.searchNode(data: 9)?.data)
 
-intList.remove(index: 1)
-// 13456789
-intList.insert(data: 10000, index: 4)
-// 1345 10000 6789
+intList.remove(at: 1)
+let removeData = intList.searchNode(data: 0)
+print(removeData?.data, removeData?.next?.data, removeData?.next?.next?.data) // 0, 1
 
-print(intList.searchNode(data: 10000)?.next?.data)
-print(intList.searchNode(data: 9)?.data)
+intList.remove(at: 2) // index를 초과했기 때문에 값을 지우지 않음
+let removeData2 = intList.searchNode(data: 0)
+print(removeData2?.data, removeData2?.next?.data, removeData2?.next?.next?.data) // 0, 1
 
+intList.remove(at: 0)
+let headRemove = intList.searchNode(data: 1) // 1
+print(headRemove?.data, headRemove?.next?.data)
+
+intList.removeLast()
+print(intList.searchNode(data: 1)?.data) // nil
+
+
+/// 이전 공부 기록
+//class Node<T: Equatable> {
+//    var data: T?
+//    var next: Node?
+//    
+//    init(data: T, next: Node? = nil) {
+//        self.data = data
+//        self.next = next
+//    }
+//    
+//    deinit {
+//        print("DEINIT--\(data)")
+//    }
+//}
+//
+//class LinkedList<T: Equatable> {
+//    private var head: Node<T>?
+//    
+//    func append(data: T) {
+//        // head가 없을 때
+//        guard let _ = self.head else {
+//            self.head = Node(data: data)
+//            return
+//        }
+//        
+//        // head를 기본으로 두고 연결 list의 끝 == node.next가 nil일 때까지 순회
+//        var node = self.head
+//        while node?.next != nil {
+//            node = node?.next
+//        }
+//        let newNode = Node(data: data)
+//        node?.next = newNode
+//    }
+//    
+//    func insert(data: T, index: Int) {
+//        guard let _ = self.head else {
+//            self.head = Node(data: data)
+//            return
+//        }
+//        
+//        var node = self.head
+//        // index에서 1을 뺀 만큼 순회
+//        for _ in 0 ..< (index - 1) {
+//            if node?.next == nil {break}
+//            node = node?.next
+//        }
+//        let nextNode = node?.next
+//        node?.next = Node(data: data)
+//        node?.next?.next = nextNode
+//    }
+//    
+//    func removeLast() {
+//        guard self.head != nil else {return}
+//        guard self.head?.next != nil else { self.head = nil; return}
+//        
+//        var node = self.head
+//        // node의 마지막 값까지는 들어가지 않도록 > 자기 자신을 지울 수 없음
+//        while node?.next?.next != nil {
+//            node = node?.next
+//        }
+//        node?.next = nil
+//    }
+//    
+//    func remove(index: Int) {
+//        guard self.head != nil else {return}
+//        guard index != 0 || self.head?.next == nil else {
+//            self.head = self.head?.next
+//            return
+//        }
+//        
+//        var node = self.head
+//        for _ in 0 ..< (index - 1) {
+//            if node?.next?.next == nil {break}
+//            node = node?.next
+//        }
+//        
+//        let nextValue = node?.next?.next
+//        node?.next = nextValue
+//    }
+//    
+//    func searchNode(data: T) -> Node<T>? {
+//        guard self.head != nil else {return nil}
+//        guard self.head?.data != data else {return self.head}
+//        
+//        var node = self.head
+//        while node?.next != nil {
+//            if node?.data == data {
+//                break
+//            }
+//            node = node?.next
+//        }
+//        
+//        return node
+//    }
+//}
+//
+//var intList = LinkedList<Int>()
+//
+//for item in 1 ... 10 {
+//    intList.append(data: item)
+//}
+//
+//print(intList.searchNode(data: 5)?.data)
+//
+//intList.removeLast()
+//
+//print(intList.searchNode(data: 9)?.data)
+//
+//intList.remove(index: 1)
+//// 13456789
+//intList.insert(data: 10000, index: 4)
+//// 1345 10000 6789
+//
+//print(intList.searchNode(data: 10000)?.next?.data)
+//print(intList.searchNode(data: 9)?.data)
+//
