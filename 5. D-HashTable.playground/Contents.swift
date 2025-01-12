ca9b20d08b62c05f7f2374bdc42de3ce79c725ce
YSLee-Dev <ysleedev01@gmail.com>
2025-01-12 20:19:50 +0900

    Study: HashTable 정의 추가 (복습)

--- 5. D-HashTable.playground/Contents.swift
+++ 5. D-HashTable.playground/Contents.swift
@@ -1,5 +1,17 @@
 import UIKit
 
+/// 5. HashTable
+/// Key와 Value로 데이터를 저장하며, Key를 입력하면 맞는 Value가 나오는 형태의 자료구조
+/// - Swift의 딕셔너리와 유사하며, 실제로 딕셔너리도 내부에서 HashTable을 이용하여 구성이 되어 있음
+/// - 자료의 순서를 보장하지 않음
+///
+/// HashTable은 내부에 배열을 통해 값을 저장하고 있음
+/// Key와 Value을 저장하면, Hash 함수를 통해 Key를 Hash하고, Hash 주소 값에 해당하는 HashTable 슬롯에 데이터를 저장함
+/// - Key 값만으로 배열의 index를 확인하고, 데이터를 가져올 수 있어야함
+///
+/// Key값을 항상 고유의 Hash 주소로 만들어 주는 함수를 Hash 함수라고함
+/// 해당 함수를 이용하여 Key는 Hash 주소로 변환 후, HashTable의 특정 주소의 테이블 값에 접근하는 것
+
 struct HashTable<T: Equatable> {
     private var hashTable: [T?] = .init(repeating: nil, count: 4)
     
