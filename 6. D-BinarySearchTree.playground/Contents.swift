c8631a5254886bdcfc3726f0b499816130c62782
YSLee-Dev <ysleedev01@gmail.com>
2025-01-15 21:12:27 +0900

    Study: BST 정의 추가 (복습)

--- 6. D-BinarySearchTree.playground/Contents.swift
+++ 6. D-BinarySearchTree.playground/Contents.swift
@@ -1,5 +1,42 @@
 import UIKit
 
+/// 6. BST
+/// Tree란?
+/// Node와 Branch를 이용하여 사이클을 이루지 않도록 구성한 데이터 구조
+/// - Node를 사용하기 때문에 LinkedList를 구현함 (Heap도 가능)
+///
+/// LinkedList와 Tree는 동일하게 Node를 통해 데이터를 구성
+/// 단, LinkedList는 prev, next를 통해 데이터를 선형적으로 관리하지만,
+/// Tree는 최상단을 기준으로 왼쪽, 오른쪽에 자식이 배치되는 비선형적 구조를 가지고 있음
+/// - 자식이 배치됨에 따라 Tree의 Node는 계층을 가지고 구성됨
+///
+/// 자식의 Node를 가르키는 선을 Branch(edge)라고 함
+/// - 현 Node의 다음 주소 값은 내 자식 주소 값이 됨
+/// Branch는 아래로만 뻗을 수 있기 때문에 삼각형 구조가 되지 못함
+///
+/// Tree 용어
+/// Node: Tree에서 데이터를 구성하고 있는 요소
+/// RootNode: 최상위 Node
+/// Level: Node의 깊이
+/// ParentNode: 나를 가르키는 내 윗 Level의 Node
+/// ChildNode: 내가 가르키는 내 아래 Level의 Node
+/// LeafNode: ChildNode가 없는 Node
+/// Depth: Tree에서 Node가 가질 수 있는 최대 깊이
+///
+/// 이진트리
+/// Tree의 Branch는 여러개로 이루어질 수 있음
+/// Branch의 개수를 0~2개로 제한한 Node로만 이루어진 Tree를 이진트리라고 함
+///
+/// 이진 탐색 트리
+/// 이진트리에 더불어 아래 규칙을 준수하고 있는 트리를 말함
+///
+/// 1. Node의 데이터에 Nil 값이 올 수 없음
+/// 2. Node의 데이터는 고유해야함 (중복이 올 수 없음)
+/// 3. Node의 데이터를 기준으로 자신의 Node 데이터보다 큰 값은 오른쪽, 작은 값은 왼쪽 Node에 위치시킴 (Child)
+///
+/// 이진 탐색 트리는 BST로 부르기도 함
+///
+
 // node 구성
 class Node<T: Comparable> {
     var data: T
